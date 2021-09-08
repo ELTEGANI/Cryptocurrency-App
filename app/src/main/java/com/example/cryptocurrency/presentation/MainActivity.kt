a11bdce0a3707f6395486520a2f778fe13f356ed
altegani mohammed <tiganimoh@gmail.com>
2021-09-08 15:53:12 +0200

    add screaming structure

--- app/src/main/java/com/example/cryptocurrency/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cryptocurrency
+package com.example.cryptocurrency.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,13 +8,13 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.cryptocurrency.ui.theme.CryptocurrencyTheme
+import com.example.cryptocurrency.presentation.theme.CryptocurrencyAppYTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CryptocurrencyTheme {
+            CryptocurrencyAppYTTheme() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
@@ -32,7 +32,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CryptocurrencyTheme {
+    CryptocurrencyAppYTTheme() {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/example/cryptocurrency/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/cryptocurrency/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.cryptocurrency.ui.theme
+package com.example.cryptocurrency.presentation.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
@@ -8,21 +8,29 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    h1 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+    ),
+    h2 = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp
+    ),
+    h4 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    body2 = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 14.sp
     )
-    */
 )
