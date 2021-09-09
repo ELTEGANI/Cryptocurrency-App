02147babd4661649225ba4ae9be41d8f400cc1ef
altegani mohammed <tiganimoh@gmail.com>
2021-09-09 20:08:17 +0200

    complete compose ui

---
+++ app/src/main/java/com/example/cryptocurrency/presentation/coin_details/components/TeamListItem.kt
@@ -0,0 +1,33 @@
+package com.example.cryptocurrency.presentation.coin_details.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import com.example.cryptocurrency.data.remote.dto.TeamMember
+
+
+@Composable
+fun TeamListItem(
+    teamMember: TeamMember,
+    modifier: Modifier = Modifier
+){
+    Column (
+      modifier = modifier,
+      verticalArrangement = Arrangement.Center
+    ){
+       Text(
+           text = teamMember.name,
+           style = MaterialTheme.typography.h4
+       )
+       Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = teamMember.position,
+            style = MaterialTheme.typography.body2,
+            fontStyle = FontStyle.Italic
+        )
+    }
+}
